fe05f2057c92764932e51777c19ede619339cdba
Turarbek-2005 <turarbek.kabdesh@mail.ru>
2024-07-15 15:34:55 +0500

    fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,37 +1,37 @@
 <script setup lang="ts">
-import { account } from "@/utils/appwrite";
-import { useAuthStore, useIsLoadingStore } from "~/store/auth.store";
+import { account } from '@/utils/appwrite';
+import { useAuthStore, useIsLoadingStore } from '~/store/auth.store';
 
 const isLoadingStore = useIsLoadingStore();
 const store = useAuthStore();
 const router = useRouter();
 
 onMounted(async () => {
-  try {
-    const user = await account.get();
-    if (user) store.set(user);
-  } catch (error) {
-    router.push("/login");
-  } finally {
-    isLoadingStore.set(false);
-  }
+    try {
+        const user = await account.get();
+        if (user) store.set(user);
+    } catch (error) {
+        router.push('/login');
+    } finally {
+        isLoadingStore.set(false);
+    }
 });
 </script>
 
 <template>
-  <LayoutLoader v-if="isLoadingStore.isLoading" />
-  <section v-else :class="{ grid: store.isAuth }">
-    <LayoutSidebar v-if="store.isAuth" />
-    <div>
-      <slot></slot>
-    </div>
-  </section>
+    <LayoutLoader v-if="isLoadingStore.isLoading" />
+    <section v-else :class="{ grid: store.isAuth }">
+        <LayoutSidebar v-if="store.isAuth" />
+        <div>
+            <slot></slot>
+        </div>
+    </section>
 </template>
 
 <style scoped>
 .grid {
-  display: grid;
-  grid-template-columns: 1fr 6fr;
-  min-height: 100vh;
+    display: grid;
+    grid-template-columns: 1fr 6fr;
+    min-height: 100vh;
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  modules: [
-    "@nuxt/image",
-    "shadcn-nuxt",
-    "@nuxt/ui",
-    "nuxt-icon",
-    "@pinia/nuxt",
-    [
-      "@nuxtjs/google-fonts",
-      {
-        families: {
-          Lato: {
-            wght: [300, 400, 700],
-            ital: [300],
-          },
-        },
-      },
+    devtools: { enabled: false },
+    modules: [
+        '@nuxt/image',
+        'shadcn-nuxt',
+        '@nuxt/ui',
+        'nuxt-icon',
+        '@pinia/nuxt',
+        [
+            '@nuxtjs/google-fonts',
+            {
+                families: {
+                    Lato: {
+                        wght: [300, 400, 700],
+                        ital: [300],
+                    },
+                },
+            },
+        ],
+        [
+            '@vee-validate/nuxt',
+            {
+                autoImports: true,
+            },
+        ],
     ],
-    [
-      "@vee-validate/nuxt",
-      {
-        autoImports: true,
-      },
-    ],
-  ],
-  shadcn: {
-    prefix: "Ui",
-    componentDir: "./components/ui",
-  },
-  pinia: {
-    storesDirs: ["./store/**"],
-  },
+    shadcn: {
+        prefix: 'Ui',
+        componentDir: './components/ui',
+    },
+    pinia: {
+        storesDirs: ['./store/**'],
+    },
 });
